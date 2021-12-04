0dd827d5cbcc6485bb72b0a178a060dc6696e2b9
BoNGoNAWT <58984645+BoNGoNAWT@users.noreply.github.com>
2021-12-04 20:28:05 +0200

    Update fungus.js

--- commands/info/fungus.js
+++ commands/info/fungus.js
@@ -15,7 +15,7 @@ module.exports = {
             .setImage('https://i.ytimg.com/vi/pa2COtLKexg/maxresdefault.jpg')
             .setTimestamp()
             .setFooter(`DARK SIDE`, message.guild.iconURL({dynamic: true}))
-            message.channel.send(funn)
+            message.channel.send(funn);
         }else{
         var fun = new MessageEmbed()
         .setAuthor('Грибомания', 'https://media.baamboozle.com/uploads/images/230671/1617448971_127957_gif-url.gif')
@@ -23,11 +23,11 @@ module.exports = {
         .setImage('https://steamuserimages-a.akamaihd.net/ugc/782992988810494373/7E44E576E44B18284B5DADFA3CB55C80F4970EF8/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')
         .setTimestamp()
         .setFooter(`DARK SIDE`, message.guild.iconURL({dynamic: true}))
+        message.channel.send(fun)
         message.channel.send(`<@${user.id}>`).then(message => {
             message.delete({ timeout: 10000 })
           })
           .catch(console.error);
-        message.channel.send(fun);
-        } return;
+        }
     }
 }
