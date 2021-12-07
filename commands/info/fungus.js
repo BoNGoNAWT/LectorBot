dacfd27c13dc74e9880aaa8363eb547e9edb6f52
BoNGoNAWT <58984645+BoNGoNAWT@users.noreply.github.com>
2021-12-07 13:48:47 +0200

    Add files via upload

--- commands/info/fungus.js
+++ commands/info/fungus.js
@@ -1,1 +1,33 @@
+const { MessageEmbed } = require("discord.js");
 
+module.exports = {
+    name: "fungus",
+    category: "info",
+    descripption: "have a fungus",
+    run: async (client, message, args) => {
+        message.delete();
+        let user = message.mentions.users.first();
+        if (!user) return message.reply('Укажите тег');
+        if(user.id === message.author.id){
+            var funn = new MessageEmbed()
+            .setAuthor('Грибомания', 'https://media.baamboozle.com/uploads/images/230671/1617448971_127957_gif-url.gif')
+            .setDescription(`${message.author} обожрался грибов`)
+            .setImage('https://i.ytimg.com/vi/pa2COtLKexg/maxresdefault.jpg')
+            .setTimestamp()
+            .setFooter(`DARK SIDE`, message.guild.iconURL({dynamic: true}))
+            message.channel.send(funn)
+        }else{
+        var fun = new MessageEmbed()
+        .setAuthor('Грибомания', 'https://media.baamboozle.com/uploads/images/230671/1617448971_127957_gif-url.gif')
+        .setDescription(`${message.author} подарил гриб пользователю ${user}`)
+        .setImage('https://steamuserimages-a.akamaihd.net/ugc/782992988810494373/7E44E576E44B18284B5DADFA3CB55C80F4970EF8/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')
+        .setTimestamp()
+        .setFooter(`DARK SIDE`, message.guild.iconURL({dynamic: true}))
+        message.channel.send(`<@${user.id}>`).then(message => {
+            message.delete({ timeout: 10000 })
+          })
+          .catch(console.error);
+        message.channel.send(fun);
+        }
+    }
+}
